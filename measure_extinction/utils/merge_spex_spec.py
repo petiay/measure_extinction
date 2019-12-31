81fdaa5297d780dc656703de701e02af4b41f07e
Marjorie Decleir <mdecleir@stsci.edu>
2019-12-31 15:48:02 -0500

    changed path to make it more general

--- measure_extinction/utils/merge_spex_spec.py
+++ measure_extinction/utils/merge_spex_spec.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import argparse
 import numpy as np
+import pkg_resources
 
 from astropy.table import Table
 
@@ -18,7 +19,8 @@ if __name__ == "__main__":
     parser.add_argument(
         "--path",
         help="path where merged spectra will be stored",
-        default="/home/mdecleir/Documents/NIR_ext/Data/Reduced",
+        default=pkg_resources.resource_filename('measure_extinction',
+                                            'data/')
     )
     parser.add_argument("--outname", help="Output filebase")
     parser.add_argument("--png", help="save figure as a png file", action="store_true")
@@ -26,7 +28,7 @@ if __name__ == "__main__":
     parser.add_argument("--pdf", help="save figure as a pdf file", action="store_true")
     args = parser.parse_args()
 
-    sfilename = "%s/%s.txt" % (args.path, args.starname)
+    sfilename = "%sOrig/NIR/%s.txt" % (args.path, args.starname)
     stable = Table.read(
         sfilename,
         format="ascii",
@@ -43,5 +45,5 @@ if __name__ == "__main__":
         outname = args.outname
     else:
         outname = args.starname
-    spex_file = "%s_table.fits" % (outname)
-    rb_stis_opt.write("%s/%s" % (args.path, spex_file), overwrite=True)
+    spex_file = "%s_spex_table.fits" % (outname)
+    rb_stis_opt.write("%s/Out/%s" % (args.path, spex_file), overwrite=True)
