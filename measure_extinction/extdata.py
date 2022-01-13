11f0ac4cddebbbdb198bad214b6805b11461caf2
Karl D. Gordon <kgordon@stsci.edu>
2022-01-13 10:46:17 -0500

    fix typo

--- measure_extinction/extdata.py
+++ measure_extinction/extdata.py
@@ -690,7 +690,7 @@ class ExtData:
                 if np.sum(zvals) > 0:
                     self.uncs[curname][zvals] = fullav[1] / fullav[0]
 
-                # make sure measuremnts with npts = 0 have zero exts and uncs
+                # make sure measurements with npts = 0 have zero exts and uncs
                 zvals = self.npts[curname] <= 0
                 if np.sum(zvals) > 0:
                     self.exts[curname][zvals] = 0.0
