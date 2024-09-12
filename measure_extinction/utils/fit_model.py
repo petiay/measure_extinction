ac6280e9a0cc25a39a1f713389cc2e24ebcef334
Karl Gordon <kgordon@stsci.edu>
2024-09-12 14:58:22 -0400

    codestyle error

--- measure_extinction/utils/fit_model.py
+++ measure_extinction/utils/fit_model.py
@@ -241,12 +241,12 @@ def get_best_fit_params(sampler):
             (indxs,) = np.where(sampler.lnprobability[k] == tmax_lnp)
             fit_params_best = sampler.chain[k, indxs[0], :]
 
-    #ndim = len(fit_params_best)
-    #params_best = np.zeros((ndim + 3))
-    #params_best[0:ndim] = fit_params_best
-    #params_best[ndim] = params_best[3] / params_best[4]
-    #params_best[ndim + 1] = (10 ** params_best[10]) / params_best[3]
-    #params_best[ndim + 2] = (10 ** params_best[10]) / params_best[ndim]
+    # ndim = len(fit_params_best)
+    # params_best = np.zeros((ndim + 3))
+    # params_best[0:ndim] = fit_params_best
+    # params_best[ndim] = params_best[3] / params_best[4]
+    # params_best[ndim + 1] = (10 ** params_best[10]) / params_best[3]
+    # params_best[ndim + 2] = (10 ** params_best[10]) / params_best[ndim]
 
     return fit_params_best
 
