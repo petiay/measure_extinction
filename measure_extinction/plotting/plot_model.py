3552e16dc82171dd9691ca86002ec4c076c81c15
Karl Gordon <kgordon@stsci.edu>
2025-04-11 10:55:51 -0400

    adding plotting from chains

---
+++ measure_extinction/plotting/plot_model.py
@@ -0,0 +1,100 @@
+import argparse
+import pickle
+import emcee
+import matplotlib.pyplot as plt
+import numpy as np
+import astropy.units as u
+
+from measure_extinction.model import MEModel
+from measure_extinction.stardata import StarData
+from measure_extinction.extdata import ExtData
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("starname", help="Name of star")
+    parser.add_argument(
+        "--plttype",
+        help="Type of plot",
+        choices=["chains", "corner", "bestfit"],
+        default="bestfit",
+    )
+    parser.add_argument("--burnfrac", help="burn fraction", default=0.5, type=float)
+    parser.add_argument(
+        "--obspath", help="path to observed data", default="/home/kgordon/Python/extstar_data/MW/"
+    )
+    parser.add_argument(
+        "--picmodname", help="name of pickled model", default="tlusty_z100_modinfo.p"
+    )
+    args = parser.parse_args()
+
+    # base filename
+    outname = f"figs/{args.starname}_mefit"
+
+    # get the observed data
+    fstarname = f"{args.starname}.dat"
+    reddened_star = StarData(fstarname, path=f"{args.obspath}")
+
+    # get the modeling info
+    modinfo = pickle.load(open(args.picmodname, "rb"))
+
+    # setup the ME model
+    memod = MEModel(obsdata=reddened_star, modinfo=modinfo)
+
+    # get the extinction curve
+    extfile = f"{outname.replace("figs", "exts")}_ext.fits"
+    ext = ExtData(filename=extfile)
+    ptab = ext.fit_params["MCMC"]
+    for k, cname in enumerate(ptab["name"]):
+        param = getattr(memod, cname)
+        param.value = ptab["value"][k]
+        if ptab["fixed"][k] > 0.1:
+            param.fixed = True
+        else:
+            param.fixed = False
+        if ptab["prior"][k] > 0.1:
+            param.prior = (ptab["prior_val"][k], ptab["prior_unc"][k])
+        else:
+            param.prior = None
+
+    # weights
+    memod.fit_weights(reddened_star)
+
+    # get the MCMC chains
+    sampfile = f"{outname.replace("figs", "exts")}_.h5"
+    reader = emcee.backends.HDFBackend(sampfile)
+    samples = reader.get_chain()
+    nsteps = samples.shape[0]
+
+    flat_samples = reader.get_chain(discard=int(args.burnfrac * nsteps), flat=True)
+
+    # get the 50 percentile and +/- uncertainties
+    params_per = map(
+        lambda v: (v[1], v[2] - v[1], v[1] - v[0]),
+        zip(*np.percentile(flat_samples, [16, 50, 84], axis=0)),
+    )
+
+    # now package the fit parameters into two vectors, averaging the +/- uncs
+    n_params = samples.shape[2]
+    params_p50 = np.zeros(n_params)
+    params_unc = np.zeros(n_params)
+    for k, val in enumerate(params_per):
+        params_p50[k] = val[0]
+        params_unc[k] = 0.5 * (val[1] + val[2])
+
+    # set the best fit parameters in the output model
+    memod.fit_to_parameters(params_p50, uncs=params_unc)
+    print(f"p50 parameters, burnfrac={args.burnfrac}")
+    memod.pprint_parameters()
+
+    if args.plttype == "chains":
+        memod.plot_sampler_chains(reader)
+    elif args.plttype == "corner":
+        memod.plot_sampler_corner(flat_samples)
+    else:
+        memod.plot(reddened_star, modinfo)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
